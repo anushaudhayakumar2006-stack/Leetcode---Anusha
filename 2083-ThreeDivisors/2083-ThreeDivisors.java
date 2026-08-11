8692ad1c516369b7c9a3ded2823aaad553838b50
anushaudhayakumar2006-stack <anushaudhayakumar2006@gmail.com>
2026-08-11 16:03:35 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.java
@@ -0,0 +1,16 @@
+// Last updated: 8/11/2026, 4:04:31 PM
+class Solution {
+    public boolean isThree(int n) {
+        int root = (int) Math.sqrt(n);
+        if (root * root != n) return false;
+        return isPrime(root);
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
