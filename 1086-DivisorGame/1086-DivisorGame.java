a9ae220f96d5c3e08ce50069d94cb4b2b28afda5
anushaudhayakumar2006-stack <anushaudhayakumar2006@gmail.com>
2026-08-11 16:04:56 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 4:05:52 PM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+
+        }
+
+        
+    }
+}
