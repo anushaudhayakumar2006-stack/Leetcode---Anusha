1f689aff8f6c3053b8be8235da5f34a5f24baa18
anushaudhayakumar2006-stack <anushaudhayakumar2006@gmail.com>
2026-08-11 16:06:10 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 4:07:06 PM
+class Solution {
+    public String toLowerCase(String s) {
+
+        // Manual ASCII conversion
+
+        String result = "";
+
+        for(int i = 0; i < s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if(65 <= ch && ch <= 90){
+                result += (char)(ch + 32);
+            }
+            else{
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+}
